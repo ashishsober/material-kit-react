af7fd947059829460037a26fd3726277e4b63300
Gupta, Ashish <gupta.ashish@optum.com>
2024-02-05 16:54:10 +0530

    Update loginHistory.tsx

--- src/pages/loginHistory.tsx
+++ src/pages/loginHistory.tsx
@@ -1,29 +1,31 @@
-import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import * as React from 'react';
 import { useCaseQueryData } from '../utils/use-query-hooks';
 // import { getData } from '../utils/http-service'
 
 export default function loginHistory() {
-    const {data:userDetailData} = useCaseQueryData("https://citric-banner-411512.uc.r.appspot.com/application/userDetails","user-detail-data")
-    
-    React.useEffect(()=>{
-        console.log(userDetailData);
-    },[userDetailData])
+    const { data: userDetailData } = useCaseQueryData("https://citric-banner-411512.uc.r.appspot.com/application/userDetails", "user-detail-data")
 
-    const rows: GridRowsProp = [
-        { id: 1, col1: 'Hello', col2: 'World' },
-        { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-        { id: 3, col1: 'MUI', col2: 'is Amazing' },
-    ];
+    React.useEffect(() => {
+        console.log(userDetailData);
+    }, [userDetailData])
 
     const columns: GridColDef[] = [
-        { field: 'col1', headerName: 'Column 1', width: 150 },
-        { field: 'col2', headerName: 'Column 2', width: 150 },
+        { field: 'name', headerName: 'Name', width: 150, flex: 1 ,sortable:true},
+        { field: 'emailid', headerName: 'Email ID', width: 150, flex: 2 ,sortable:true},
+        { field: 'discription', headerName: 'Description', width: 150, flex: 2 ,sortable:true},
+        { field: 'profileImage', headerName: 'ProfileImage', width: 150, flex: 2 ,sortable:true},
+        { field: 'position', headerName: 'Position', width: 150, flex: 2 ,sortable:true},
+
     ];
 
     return (
-        <div style={{ height: 350, width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} />
-        </div>
+        <>{
+            userDetailData && (
+            <div style={{ height: 350, width: '100%' }}>
+                <DataGrid rows={userDetailData} columns={columns} />
+            </div>
+        )}
+        </>
     )
 }
